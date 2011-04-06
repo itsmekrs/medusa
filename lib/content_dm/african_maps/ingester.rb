ca8fab13eab54fdca892603d0abd1436305bab93
Howard Ding <hding2@illinois.edu>
2011-04-06 10:51:58 -0700

    renamed to Ingester, as that's a better reflection of its function

---
+++ lib/content_dm/african_maps/ingester.rb
@@ -0,0 +1,10 @@
+module ContentDm
+  module AfricanMaps
+    class Ingester
+
+      #Read collection information and create collection object via AF
+      #For each item read item information and create item object via AF
+      #Output collection/item PIDS etc. for reference
+    end
+  end
+end
