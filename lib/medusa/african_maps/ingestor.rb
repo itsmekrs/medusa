46df06a1ab556b80d7d535b8cee3c9f1e3c877d4
Howard Ding <hding2@illinois.edu>
2012-02-16 12:55:44 -0600

    simplify a bit more for now

--- lib/medusa/african_maps/ingestor.rb
+++ lib/medusa/african_maps/ingestor.rb
@@ -17,11 +17,11 @@ module Medusa
         fedora_collection = nil
         replacing_object(collection.medusa_id) do
           fedora_collection = Medusa::AfricanMaps::Object.new(:pid => collection.medusa_id)
-          premis_ds = ActiveFedora::Datastream.new(:dsLabel => 'PREMIS',
-                                                   :controlGroup => "X",
-                                                   :blob => File.open(collection.premis_file))
-          premis_ds.dsid = 'PREMIS'
-          fedora_collection.add_datastream(premis_ds)
+          #premis_ds = ActiveFedora::Datastream.new(:dsLabel => 'PREMIS',
+          #                                         :controlGroup => "X",
+          #                                         :blob => File.open(collection.premis_file))
+          #premis_ds.dsid = 'PREMIS'
+          #fedora_collection.add_datastream(premis_ds)
           fedora_collection.save
         end
         puts "INGESTED COLLECTION: #{collection.medusa_id}"
